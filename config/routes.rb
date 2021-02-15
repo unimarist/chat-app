7a738b30acac906c315d3a9909d173ff88b6f368
unimarist <daison7@icloud.com>
2021-02-15 19:13:50 +0900

    メッセージ送信実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "rooms#index"
-  resources :messages, only: [:index]
+  resources :messages, only: [:index, :create]
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new , :create]
-
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
